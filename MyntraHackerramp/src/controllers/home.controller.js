4f9bb4f7145b6faadb82c2bbc5deb3d919c20689
unknown <ayushsalvi19@gmail.com>
2021-11-03 00:57:36 +0530

    Add Influencer Data

--- MyntraHackerramp/src/controllers/home.controller.js
+++ MyntraHackerramp/src/controllers/home.controller.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const product =require(__dirname+"/../models/product.model.js");
+// const product =require(__dirname+"/../models/product.model.js");
 const productDataArray=require(__dirname+"/../productData.js");
+const influencerDataArray=require(__dirname+"/../influencerData.js");
 const router = express.Router();
+// const influencerDataArray=require(__dirname+"/../influencerData.js");
 
-const Product=product.modal;
+
 
 router.get("/", async (req, res) => {
   console.log(productDataArray);
-  return res.render("ejs/home", {productDataArray:productDataArray});
+  return res.render("ejs/home", {productDataArray:productDataArray,influencerDataArray:influencerDataArray});
 });
 
 router.get("/user/:id", async (req, res) => {
